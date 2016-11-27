f0f39da00d45bd75761b4be2b0951b6511911351
Mocha-- <xibowangmelb@gmail.com>
2016-11-27 20:27:42 +1100

    change(tokenRouter): change for ci testing

--- Routes/TokenRouter.js
+++ Routes/TokenRouter.js
@@ -4,7 +4,7 @@ const tokenRouter = express.Router();
 tokenRouter.route('/token')
     .get((req, res) => {
         res.send({
-            token: 'lalala'
+            token: 'lalala1'
         });
     });
 
