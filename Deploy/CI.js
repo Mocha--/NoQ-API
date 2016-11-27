ba16a2e0d9a5168a6c4ee9e7c419375c3c4b4bd7
Mocha-- <xibowangmelb@gmail.com>
2016-11-27 20:28:31 +1100

    change(CIScript): add log for every step

--- Deploy/CI.js
+++ Deploy/CI.js
@@ -4,36 +4,51 @@ const path = require('path');
 const nodessh = new SSH();
 
 const printLog = (log) => {
+    console.info('');
     console.info('*'.repeat(log.length + 6));
     console.info(`*  ${log}  *`);
     console.info('*'.repeat(log.length + 6));
 }
 
-printLog('Your code will be pushed to origin repository, dev branch.');
+printLog('1. Pusing code to origin repository, dev branch.');
 
 shell.exec('git push origin dev', {silent: true}, (code, stdout, stderr) => {
+    printLog('Push successfully.');
+    printLog('2. Connecting to your server.')
     nodessh
         .connect({
             host: 'www.mooocha.com',
             username: 'ubuntu',
             privateKey: path.join(__dirname, './NoQ.key')
         })
+        .catch((rej) => {
+            printLog('Can not connect to your server, due to:')
+            console.info(rej);
+            Prosime.reject();
+        })
         .then(() => {
-            console.info('success connect')
+            printLog('Connect successfully.')
+            printLog('3. Remote server is pulling latest code from origin repository, dev branch.')
             return nodessh.execCommand('git pull origin dev', {cwd: '/home/ubuntu/www/NoQ-API'})
         })
+        .catch((rej) => {
+            printLog('Remote server failed pulling latest code, due to:');
+            console.info(rej);
+            Promise.reject();
+        })
         .then((res) => {
-            console.info(res)
+            printLog('Remote server pulled latest code.')
+            printLog('4. Remote server is restarting api server.')
             return nodessh.execCommand('pm2 restart Server.js', {cwd: '/home/ubuntu/www/NoQ-API'})
         })
-        .then((res) => {
-            console.info(res)
-            nodessh.dispose();
-        })
         .catch((rej) => {
+            printLog('Remote server failed restarting api server, due to:');
             console.info(rej);
             nodessh.dispose();
         })
-
+        .then((res) => {
+            printLog('Api server has been restarted.')
+            nodessh.dispose();
+        });
 });
 
